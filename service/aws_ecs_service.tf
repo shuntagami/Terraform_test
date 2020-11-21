e11f9510842b34d1806ba9f70d8fccca3cf11dc6
shuntagami <shuntagami23@gmail.com>
2020-11-21 10:23:05 +0900

    ロードバランサーの作成

--- service/aws_ecs_service.tf
+++ service/aws_ecs_service.tf
@@ -4,4 +4,14 @@ resource "aws_ecs_service" "webapp-service" {
   task_definition = aws_ecs_task_definition.sample-task.arn
   desired_count   = 1
   launch_type     = "EC2"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.http.arn
+    container_name   = "sample-webapp"
+    container_port   = "4567"
+  }
 }
+
+# depends_on = [
+#   "aws_alb_listener.web_front_end",
+# ]
