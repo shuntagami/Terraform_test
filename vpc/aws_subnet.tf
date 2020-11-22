7cd45dae0c2c6630ff7a18a7d3ad232a26b0e8e2
shuntagami <shuntagami23@gmail.com>
2020-11-22 12:19:22 +0900

    RDS用プライベートサブネット作成

--- vpc/aws_subnet.tf
+++ vpc/aws_subnet.tf
@@ -15,3 +15,15 @@ resource "aws_subnet" "public_subnet_2" {
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-1c"
 }
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-northeast-1c"
+}
