8b7a8e75e38aeece05b5fa616af2fa6db77757a2
shuntagami <shuntagami23@gmail.com>
2020-11-26 21:16:35 +0900

    ロードバランサにssl証明書をセット

--- service/aws_lb_listener.tf
+++ service/aws_lb_listener.tf
@@ -2,8 +2,27 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = "${aws_lb.lb.arn}"
   port              = "80"
   protocol          = "HTTP"
+
   default_action {
-    target_group_arn = "${aws_lb_target_group.http.arn}"
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = "${aws_lb.lb.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = "${aws_acm_certificate.touringtaro.arn}"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
     type             = "forward"
+    target_group_arn = "${aws_lb_target_group.http.arn}"
   }
 }
